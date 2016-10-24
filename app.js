67a982307fb66fe9d0e64aef09e15b6935569e6b
harishspree <harishspree@bitbucket.org>
2016-10-25 03:06:43 +0530

    random graph

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ server.listen(app.get('port'), function (){
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
-app.post('/notes', function(req, res) {
+app.post('/randgraph', function(req, res) {
   io.emit('change:graph',req.body.no); 
 });
 
